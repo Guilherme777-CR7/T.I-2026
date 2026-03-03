97bb29c49ffdbc7f3c22baaba91cd009a2578555
Guilherme777-CR7 <guilhermebatista10711@gmail.com>
2026-03-03 14:33:39 -0300

    Add files via upload

---
+++ Algoritimo python 3 do 3 guilherme batista/atv1.py
@@ -0,0 +1,9 @@
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número: "))
+
+sm = n1 + n2
+pdt = n1 * n2
+
+print(f"\nResultados:")
+print(f"A soma dos dois valores é: {sm}")
+print(f"O produto entre eles é: {pdt}")
