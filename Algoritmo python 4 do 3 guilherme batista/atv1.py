24be816d38511a69b1c5e50e1c7b0e9397f2cb0a
Guilherme777-CR7 <guilhermebatista10711@gmail.com>
2026-03-04 11:18:40 -0300

    Add files via upload

---
+++ Algoritmo python 4 do 3 guilherme batista/atv1.py
@@ -0,0 +1,7 @@
+lista = ["sabão", "pão", "leite", "bisteca", "maça"]
+print("original: ", lista)
+
+nova = input("escreva oque deseja comprar: ")
+
+lista.append(nova)
+print("atualizado:", lista)
---
+++ Algoritmo python 4 do 3 guilherme batista/listas.py
@@ -0,0 +1,25 @@
+no1 = "ana"
+no2 = "bruno"
+no3 = "caio"
+
+nomes = ["ana", "bruno", "caio"]
+print(nomes)
+
+dados = ["carol", 0, 1.70, True]
+print(dados)
+print(type(dados))
+print(type(dados[0]))
+print(type(dados[1]))
+print(type(dados[2]))
+print(type(dados[3]))
+
+lista = ["cachorro", "gato"]
+print("original: ", lista)
+lista.append("coelho")
+print("atualizado:", lista)
+
+lista.insert(1, "grilo")
+print("atualizado:", lista)
+
+lista.extend(1, "Macaco", "ovelha")
+print("lista final:", lista)
