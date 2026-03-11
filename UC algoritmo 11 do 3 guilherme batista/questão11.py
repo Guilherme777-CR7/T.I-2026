fbba8df0b09ff4f26c8798bd43bb12a5f5b7a335
Guilherme777-CR7 <guilhermebatista10711@gmail.com>
2026-03-11 20:34:47 -0300

    Update questão11.py

--- UC algoritmo 11 do 3 guilherme batista/questão11.py
+++ UC algoritmo 11 do 3 guilherme batista/questão11.py
@@ -7,3 +7,24 @@ for n in range(1, 101):
         st += n
 
 print(f"A soma de todos os números pares entre 1 e 100 é: {st}")
+
+#O codigo da 12:
+
+ta = 0
+qt = 0
+
+print("Sistema de Depósitos Bancários")
+print("Digite o valor do depósito ou '0' para encerrar.")
+
+while True:
+    v = float(input("Valor do depósito: "))
+    
+    if v == 0:
+        break
+    
+    ta += v
+    qt += 1
+
+print("-" * 35)
+print(f"Total de depósitos: R$ {ta:.2f}")
+print(f"Quantidade de transações: {qt}")
