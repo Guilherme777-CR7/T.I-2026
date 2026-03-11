e21aae8106719fb853ccf77243e4d1e069677c31
Guilherme777-CR7 <guilhermebatista10711@gmail.com>
2026-03-11 14:51:14 -0300

    Update questão11.py

--- UC algoritmo 11 do 3 guilherme batista/questão11.py
+++ UC algoritmo 11 do 3 guilherme batista/questão11.py
@@ -1,11 +1,8 @@
-
-soma_pares = 0
+sp = 0
 
 for numero in range(1, 101):
     
-    if numero % 2 == 0:
-        # Se for par, somamos o valor atual ao total acumulado
-        soma_pares = soma_pares + numero
+    if n % 2 == 0:
+        sp = sp + n
 
-# Imprime o resultado final após sair do laço
-print(f"A soma de todos os números pares de 1 a 100 é: {soma_pares}")
+print(f"A soma de todos os números pares de 1 a 100 é: {sp}")
