da03925644d9f4bcb7ead628a071133b59de5ef7
Guilherme777-CR7 <guilhermebatista10711@gmail.com>
2026-06-02 14:39:09 -0300

    Add files via upload

--- UC algoritmo 01 do 06guilherme baista/mainp.py
+++ UC algoritmo 01 do 06guilherme baista/mainp.py
@@ -2,18 +2,22 @@ from flask import Flask,render_template, request, redirect, url_for,Flash
 
 app = Flask(__name__)
 
-@app.route('/cadastro', methods=['GET', 'POST'])
+@app.route('/')
+def formulario():
+    return render_template('index.html')
+
+@app.route('/validacao', methods=['POST'])
 def cadastro():
 
-    mensagem = ""
+    nome = request.form.get('nome', '').strip().title()
+    email = request.form.get('email', '').strip().lower()
+    cidade = request.form.get('cidade', '').strip().title()
 
-    if request.method == 'POST':
-        nome = request.form.get('nome')
-        if not nome:
-            mensagem = "O campo nome é obrigatório."
-        else:
-            mensagem = f"Cadastro realizado com sucesso! Bem vindo, {nome}"
-    return render_template('cadastro.html', mensagem=mensagem)
+    return f"""
+    Nome: {nome}<br>
+    Email: {email}<br>
+    Cidade: {cidade}
+    """
 
-if __name__ == "__main__":
+if __name__ == '_main_':
     app.run(debug=True)
