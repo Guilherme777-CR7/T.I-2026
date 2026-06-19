2f639b8a030ef67dca9c2862a2278242bcc65d30
Guilherme777-CR7 <guilhermebatista10711@gmail.com>
2026-06-19 16:42:43 -0300

    Add files via upload

--- atividade em dupla 18 do 06/app.py
+++ atividade em dupla 18 do 06/app.py
@@ -1,43 +1,44 @@
-from flask import Flask, render_template, session, redirect
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
-app.secret_key = "senha123"
 
-@app.route("/")
-def inicio():
-    if "placar" not in session:
-        session["placar"] = {
-            "Time A": 0,
-            "Time B": 0,
-            "Time C": 0,
-            "Time D": 0
-        }
+# Banco de dados da Copa do Mundo (Todos começam com 0)
+copa_dados = {
+    "Países-Sede": {"Canadá": 0, "Estados Unidos": 0, "México": 0},
+    "América do Sul (CONMEBOL)": {"Argentina": 0, "Brasil": 0, "Colômbia": 0, "Equador": 0, "Paraguai": 0, "Uruguai": 0},
+    "Europa (UEFA)": {
+        "Alemanha": 0, "Áustria": 0, "Bélgica": 0, "Bósnia e Herzegovina": 0, "Croácia": 0, 
+        "Escócia": 0, "Espanha": 0, "França": 0, "Holanda": 0, "Inglaterra": 0, 
+        "Noruega": 0, "Portugal": 0, "República Tcheca": 0, "Suécia": 0, "Suíça": 0, "Turquia": 0
+    },
+    "África (CAF)": {
+        "África do Sul": 0, "Argélia": 0, "Cabo Verde": 0, "Costa do Marfim": 0, 
+        "Egito": 0, "Gana": 0, "Marrocos": 0, "RD do Congo": 0, "Senegal": 0, "Tunísia": 0
+    },
+    "Ásia (AFC)": {
+        "Arábia Saudita": 0, "Austrália": 0, "Catar": 0, "Coreia do Sul": 0, 
+        "Irã": 0, "Iraque": 0, "Japão": 0, "Jordânia": 0, "Uzbequistão": 0
+    },
+    "América do Norte, Central e Caribe (Concacaf)": {"Curaçau": 0, "Haiti": 0, "Panamá": 0},
+    "Oceania (OFC)": {"Nova Zelândia": 0}
+}
 
-    return render_template(
-        "index.html",
-        placar=session["placar"]
-    )
+@app.route('/')
+def home():
+    return render_template('index.html', confederacoes=copa_dados)
 
-@app.route("/ponto/<time>")
-def ponto(time):
-    placar = session["placar"]
+@app.route('/adicionar/<confederacao>/<pais>')
+def adicionar_ponto(confederacao, pais):
+    if confederacao in copa_dados and pais in copa_dados[confederacao]:
+        copa_dados[confederacao][pais] += 1
+    return redirect(url_for('home'))
 
-    if time in placar:
-        placar[time] += 1
+@app.route('/resetar')
+def resetar():
+    for confederacao in copa_dados:
+        for pais in copa_dados[confederacao]:
+            copa_dados[confederacao][pais] = 0
+    return redirect(url_for('home'))
 
-    session["placar"] = placar
-    return redirect("/")
-
-@app.route("/zerar")
-def zerar():
-    session["placar"] = {
-        "Time A": 0,
-        "Time B": 0,
-        "Time C": 0,
-        "Time D": 0
-    }
-
-    return redirect("/")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
